fd61c498f6f62a2d416f03ea4ada64d8bd85958a
Will Robertson <williamdrobertson42@gmail.com>
2023-08-03 17:42:20 +1000

    feat: add patient route for individual sessions

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -6,6 +6,7 @@ router.get('/sessions', sessionController.getSessions);
 router.post('/session', mustBePrac, sessionController.createSession);
 router.get('/sessions/prac/:pracId', sessionController.getSessionsByPracId);
 router.get('/sessions/patient/:patientId', mustBePracOrPatient, sessionController.getSessionsByPatientId);
+router.get('/session/patient/:sessionId', mustBePracOrPatient, sessionController.getSession);
 router.get('/session/:sessionId', mustBePrac, sessionController.getSession);
 router.put('/session/:sessionId', mustBePrac, verifyPractitionerOwnership, sessionController.updateSession);
 router.delete('/session/:sessionId', sessionController.deleteSession);
